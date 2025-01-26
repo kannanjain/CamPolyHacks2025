b04224d135279e7a4a0d93898fd046948cd4bdd5
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2025-01-25 20:13:21 -0800

    made map interactive

---
+++ Frontend/src/map.js
@@ -0,0 +1,11 @@
+
+
+// mapboxgl.accessToken ="pk.eyJ1Ijoic3VtZWRoYS1rdW4iLCJhIjoiY202Y3YzZDl5MG1qYjJsb29oZG05ZzNrdyJ9.znQxXNQe-ze2jFZbgdYcvw";
+// const Map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//     center: [-74.5, 40], // starting position [lng, lat]
+//     zoom: 9 // starting zoom
+//   });
+
+//   export default Map
