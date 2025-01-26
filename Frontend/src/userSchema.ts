c5ba9d67e14d256c4525a6bf1542acb167226aba
Rachana Raju <rachanaraju03@gmail.com>
2025-01-25 22:22:52 -0800

    updated schema and interface for location

--- Frontend/src/userSchema.ts
+++ Frontend/src/userSchema.ts
@@ -1,23 +1,21 @@
 import mongoose, {Schema} from "mongoose";
 
 interface IUser{
-    userId: string;
     name: string;
     email: string;
     interests: string[];
     visibility: boolean;
     lta: string;
-    location: GeolocationCoordinates;
+    location: {longitude: GeolocationCoordinates, latitude: GeolocationCoordinates};
 }
 
 const userSchema = new Schema<IUser>({
-    userId: {type: String, required: true},
     name: {type: String, required: false},
     email: {type: String, required: false},
     interests: {type: [String], required: false},
     visibility: {type: Boolean, required: true},
     lta: {type: String, required: false, default: ""},
-    location: {type:GeolocationCoordinates, required: false}
+    location: {longitude:{type: GeolocationCoordinates, required: false}, latitude: {type: GeolocationCoordinates, required: false}}
 })
 
 export default mongoose.models.user || mongoose.model("user", userSchema);
